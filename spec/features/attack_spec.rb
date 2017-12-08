6f5ee4dfe750b383f5c454d0c95a857f741de123
Valentina <valentina.rmo@gmail.com>
2017-12-08 15:09:03 +0000

    Refactoring feature test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,8 +1,19 @@
+require './lib/player'
+require './lib/game'
+
 feature "Attack player 2" do
   scenario 'attack and get confirmation' do
     sign_in_and_play
     click_button "Attack P2"
 
-    expect(page).to have_content("#{$player_2.name} has been attacked")
+    expect(page).to have_content("Sam has been attacked")
   end
+end
+  feature "Attack player 1" do
+    scenario 'attack and get confirmation' do
+      sign_in_and_play
+      click_button "Attack P1"
+
+      expect(page).to have_content("Vale has been attacked")
+    end
  end
